fb0d40e8e2ef6e5ded750bbd22e444090460ba1e
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-03-17 18:34:26 -0300

    feat: add app default theme to tyled-components

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,9 @@
+//styled-compoments must be imported so there is no overriding of its own properties
+import "styled-components";
+import { defaultTheme } from "../styles/themes/default";
+
+type ThemeType = typeof defaultTheme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType {}
+}
