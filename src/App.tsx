f660f4c20a461ab848a20c179cd888315d46faf8
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-03-18 14:42:05 -0300

    feat: add app routing to home and history pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { GlobalStyle } from './global'
+import { Router } from './routes/Router'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle></GlobalStyle>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="success"></Button>
-      <Button variant="danger"></Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
