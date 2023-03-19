6694a2e9bfba08d961bb1ba43125c9f1c3463f09
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-03-19 15:57:15 -0300

    fix: remove unecessary display property

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,9 +1,6 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  display: flex;
-  flex-direction: collumn;
-
   max-width: 74rem;
   height: calc(100vh - 10rem);
   margin: 5rem auto;
