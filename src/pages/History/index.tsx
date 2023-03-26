eb52c54762b00a5463b798133cd89d48b5618450
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-03-26 16:34:24 -0300

    feat: add history table general layout

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,65 @@
+import { HistoryContainer, ListContainer } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>My history</h1>
+      <ListContainer>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Clean bedroom</td>
+              <td>60 minutes</td>
+              <td>About a hour ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Clean bedroom</td>
+              <td>60 minutes</td>
+              <td>About a hour ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Clean bedroom</td>
+              <td>60 minutes</td>
+              <td>About a hour ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Clean bedroom</td>
+              <td>60 minutes</td>
+              <td>About a hour ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Clean bedroom</td>
+              <td>60 minutes</td>
+              <td>About a hour ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Clean bedroom</td>
+              <td>60 minutes</td>
+              <td>About a hour ago</td>
+              <td>Finished</td>
+            </tr>
+            <tr>
+              <td>Clean bedroom</td>
+              <td>60 minutes</td>
+              <td>About a hour ago</td>
+              <td>Finished</td>
+            </tr>
+          </tbody>
+        </table>
+      </ListContainer>
+    </HistoryContainer>
+  )
+}
