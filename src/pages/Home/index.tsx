dba3b8896b91c121a8a539cabecb277cad67d7bb
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-04-21 15:38:46 -0300

    feat: add validation in timer forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,11 +8,42 @@ import {
   StartButton,
   TaskInput,
 } from './styles'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+// interface NewCycleFormData {
+//  task: string
+//  minutesAmount: number
+// }
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().trim().min(1, 'Inform the task name'),
+  minutesAmount: zod.number().min(5).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: any): any {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action=''>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action=''>
         <FormContainer>
           <label htmlFor='task'>I will be working on</label>
           <TaskInput
@@ -20,6 +51,7 @@ export function Home() {
             placeholder='Give a name to your task or project'
             type='text'
             list='task-suggesitons'
+            {...register('task')}
           />
 
           <datalist id='task-suggesitons'>
@@ -34,6 +66,7 @@ export function Home() {
             max={60}
             placeholder='00'
             type='number'
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <label htmlFor=''>minutes.</label>
         </FormContainer>
@@ -46,7 +79,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartButton disabled type='submit'>
+        <StartButton disabled={isSubmitDisabled} type='submit'>
           <Play size={24} />
           Start
         </StartButton>
