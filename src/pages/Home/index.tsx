8163cd120f691534e0373bc62fbbcea38157add1
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-05-13 20:20:43 -0300

    refact: keeping the reset forms inside home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,14 +28,19 @@ export function Home() {
     },
   })
 
-  const { handleSubmit, watch /* reset */ } = newCycleForm
+  const { handleSubmit, watch, reset } = newCycleForm
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
+
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)} action=''>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action=''>
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
